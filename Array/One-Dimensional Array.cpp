cfbb0e22694dcf76bc776a0088ee41202910ba88
Viktor <63793560+BartFatim@users.noreply.github.com>
2020-04-17 16:59:01 +0300

    Add files via upload

---
+++ Array/One-Dimensional Array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void arrayfilling(int arr[],const int size)
+{
+	srand (time(NULL));
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+
+void printarray(int arr[], const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
+
+
+void main()
+{
+		const int SIZE = 10;
+		int arr[SIZE];
+
+		arrayfilling(arr,SIZE);
+		printarray(arr, SIZE);
+
+
+}
