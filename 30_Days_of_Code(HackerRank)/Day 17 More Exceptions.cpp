de8485cc9dec2f463d1309bc6ebe4a7636a62cf2
Viktor <63793560+BartFatim@users.noreply.github.com>
2020-05-18 11:25:06 +0300

    Day 17: More Exceptions
    Task:
    Write a Calculator class with a single method: int power(int,int). The power method takes two integers, n and p, as parameters and returns the integer result of (n)p. If either n or p is negative, then the method must throw an exception with the message: n and p should be non-negative.
    
    Note: Do not use an access modifier (e.g.: public) in the declaration for your Calculator class.
    
    Input Format:
    Input from stdin is handled for you by the locked stub code in your editor. The first line contains an integer, T , the number of test cases. Each of the T subsequent lines describes a test case in 2 space-separated integers denoting n and p, respectively.
    
    Constraints:
    No Test Case will result in overflow for correctly written code.
    
    Output Format:
    Output to stdout is handled for you by the locked stub code in your editor. There are T lines of output, where each line contains the result of (n)p as calculated by your Calculator class' power method.

---
+++ 30_Days_of_Code(HackerRank)/Day 17 More Exceptions.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+using namespace std;
+
+//Write your code here
+class Calculator {
+    friend int main();
+
+    int power(int n, int p) {
+
+        if ((n >= 0) && (p >= 0))
+            return pow(n, p);
+
+        else
+            throw invalid_argument("n and p should be non-negative");
+
+    }
+
+};
+
+int main()
+{
+    Calculator myCalculator = Calculator();
+    int T, n, p;
+    cin >> T;
+    while (T-- > 0) {
+        if (scanf("%d %d", &n, &p) == 2) {
+            try {
+                int ans = myCalculator.power(n, p);
+                cout << ans << endl;
+            }
+            catch (exception& e) {
+                cout << e.what() << endl;
+            }
+        }
+    }
+
+}
